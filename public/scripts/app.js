95e7721734539dfe7597f8798eb4d1f1d3075338
ryanthomas92 <rthoma11@villanova.edu>
2017-01-05 15:43:22 -0800

    get route completed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,9 +10,25 @@ angular
   // ^ the first argument is a string naming the controller,
   // the second argument is a function that defines the capacities
   // of the controller.
+AlbumsIndexController.$inject = ['$http']
 
-function AlbumsIndexController () {
+function AlbumsIndexController ( $http ) {
   var vm = this;
+  vm.results = null;
+
+  $http({
+    method: 'GET',
+    url: "/api/albums"
+  }).then(successCallback, errorCallback);
+
+  function successCallback(response) {
+    vm.albums = response.data;
+    console.log("Response for all albums: ", response);
+  }
+  function errorCallback(error) {
+    console.log("There is an error: ", error);
+  }
+
   vm.newAlbum = {};
 
   vm.newAlbum = {
@@ -20,18 +36,4 @@ function AlbumsIndexController () {
       artistName: 'Morrissey'
   };
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
 }
